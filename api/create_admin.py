7ea6b406c05dd2727f9ca37c9166d88d1aed198f
Amalchacko657 <amal.chacko@panasatech.com>
2026-04-12 11:20:27 +0530

    updated admin create code.

--- api/create_admin.py
+++ api/create_admin.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import User
 
 def create_admin():
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser(
-            username="admin",
-            email="your_email@gmail.com",
-            password="admin123"
-        )
+    User.objects.filter(username="admin").delete()
+
+    User.objects.create_superuser(
+        username="admin",
+        email="your_email@gmail.com",
+        password="admin123"
+    )
