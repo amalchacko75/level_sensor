d2a70bd5e9d1e8c95d83656afd7f3c090ff6e87d
Amalchacko657 <amal.chacko@panasatech.com>
2026-04-12 15:20:52 +0530

    apply migrations.

--- level_sensor/settings.py
+++ level_sensor/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+if os.environ.get('RENDER'):
+    import subprocess
+    subprocess.run(["python", "manage.py", "migrate"])
