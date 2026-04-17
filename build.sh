7593283386e556303888ce4507199dd8d3041672
Amalchacko657 <amal.chacko@panasatech.com>
2026-04-17 08:51:40 +0530

    whitenose issue fix

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 pip install -r requirements.txt
-
+python manage.py collectstatic --noinput
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput
