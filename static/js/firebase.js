3d2d77d10673565b04f8a30baaed2578dedd76a6
Amalchacko657 <amal.chacko@panasatech.com>
2026-07-10 18:49:40 +0530

    logo changes.

--- static/js/firebase.js
+++ static/js/firebase.js
@@ -188,10 +188,10 @@ onMessage(messaging, payload => {
                 payload.notification.body,
 
             icon:
-                "/static/icons/icon-192.svg",
+                "/static/icons/icon-192.png",
 
             badge:
-                "/static/icons/icon-192.svg"
+                "/static/icons/icon-192.png"
 
         }
 
